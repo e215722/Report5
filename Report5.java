8932c87a46063867d98392cf4008650d8969c067
Yuya Mekaru <e215722@ie.u-ryukyu.ac.jp>
2021-12-20 16:58:12 +0900

    step3

--- Report5.java
+++ Report5.java
@@ -1,12 +1,18 @@
 public class Report5{
     public static void main(String[] args) {
+        /*step2
         String str = null;
         try{   
             System.out.println(str.length());
         }catch(NullPointerException e){
             System.out.println("NullPointerExceptionが発生した！");
             System.out.println(e.getMessage());
-        }    
+        }
+        */ 
+
+        String str = "百二十三";
+        int value = Integer.parseInt(str);
+        
     }
     
 }
